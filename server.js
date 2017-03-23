b360bc6936dc4dfc6682f652575a26bf1828e13c
jogoshugh <jsgough@gmail.com>
2017-03-23 08:37:16 -0400

    Adjust port parsing

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.post('/pt/*', function (req, res, next) {
     
 });
 
-var port = Number(process.env.PORT || 5000);
+var port = parseInt(process.env.PORT) || 5000;
 
 app.use(express.static(__dirname + '/client'));
 
